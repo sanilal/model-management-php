ac24d404b77a8fcc5942d7c4d0a00aab00b42ed1
Sanilal P Sathyan <sanilal@gmail.com>
2024-01-09 15:25:15 +0400

    option4

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+//document ready
+	$(document).ready(function() {
+		$('.filters-button').click(function(){
+			$(this).toggleClass('active');
+			$('.filters').toggleClass('active fadeInDown fadeOutUp');
+			
+		});
+		
+		
+		
+	})
+//document ready ends
+
+
+
+
+
+(function(){
+  "use strict";
+  	$('.gallery').flickity({
+				  // options
+				  cellAlign: 'left',
+				  contain: true
+				});
+	
+	$(document).ready(function() {
+		
+	});
+	
+})();
+
+// Non-strict code... 
+
+
